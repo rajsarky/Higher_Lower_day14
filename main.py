8b2692d117c5742a85a09d9fb350a20838a145ca
rajsarky <k.n.sarkar@icloud.com>
2024-01-21 17:35:02 +0000

    Final code

--- main.py
+++ main.py
@@ -2,32 +2,50 @@ from game_data import data
 import art
 import random
 
-#TODO: from the list output the first information then ask if the second one has higher or lower follwer
-
 total_point = 0
 
 print(art.logo)
 
 
+def point_calculator(points):
+    global total_point
+    total_point += points
+
+
+def get_total_points():
+    return total_point
+
+
 def random_selection():
-    return random.choices(data, k=2)
+    return random.sample(data, k=2)
+
+
 def compare():
     random_search_terms = random_selection()
     choice1 = random_search_terms[0]
     choice2 = random_search_terms[1]
     print(f"Compare A: {choice1["name"]}, {choice1["description"]}, from {choice1["country"]}")
     print(art.vs)
-    print(print(f"Compare A: {choice2["name"]}, {choice2["description"]}, from {choice2["country"]}"))
+    print(f"Compare B: {choice2["name"]}, {choice2["description"]}, from {choice2["country"]}")
 
     while True:
-        choice = input("Who has more followers? Type 'A' or 'B' ").lower()
+        choice = input("Who has more followers? Type 'A' or 'B':   \n").lower()
         if choice == "a" or choice == "b":
             break
     if choice == "a":
         if choice1["follower_count"] > choice2["follower_count"]:
-
-
-
+            point_calculator(1)
+            print(f"You're right! Current score: {get_total_points()}")
+            compare()
+        else:
+            print(f"Sorry, that's wrong. Final score: {total_point}")
+    else:
+        if choice2["follower_count"] > choice1["follower_count"]:
+            point_calculator(1)
+            print(f"You're right! Current score: {get_total_points()}")
+            compare()
+        else:
+            print(f"Sorry, that's wrong. Final score: {total_point}")
 
 
 compare()
